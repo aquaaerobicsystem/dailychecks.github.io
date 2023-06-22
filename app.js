9d3c6ecd339ca9ada670f18f66ddbab5463bb857
AquaDevelopers - Chuck Konkol <myaquaapps@gmail.com>
2023-06-22 07:03:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getDatabase }   from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +15,10 @@ const firebaseConfig = {
   measurementId: "G-1S2R9B3YL3"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+function writeUserData(datetime,message) {
+  const db = getDatabase();
+  set(ref(db, ''log'), {
+    datetimes: datetime,
+    msg: message
+  });
+}
